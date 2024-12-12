f63a8df96acdf1fad82f53a71a4abb864741da0d
chaimaux93 <chaima.kaddouri@estiam.com>
2024-12-12 20:19:00 +0100

    Initial commit

---
+++ app/src/main/java/com/votrepackage/dao/UserDao.kt
@@ -0,0 +1,18 @@
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun insert(user: User)
+
+    @Query("SELECT * FROM user_table ORDER BY name ASC")
+    fun getAllUsers(): LiveData<List<User>>
+
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User) // Méthode ajoutée pour mettre à jour un utilisateur
+}
---
+++ app/src/main/java/com/votrepackage/utils/JsonParser.kt
@@ -0,0 +1,20 @@
+
+import android.content.Context
+import org.json.JSONObject
+
+fun parseJson(context: Context): List<User> {
+    val users = mutableListOf<User>()
+    val jsonString = context.resources.openRawResource(R.raw.users).bufferedReader().use { it.readText() }
+    val jsonArray = JSONObject(jsonString).getJSONArray("users")
+
+    for (i in 0 until jsonArray.length()) {
+        val jsonObject = jsonArray.getJSONObject(i)
+        users.add(
+            User(
+                name = jsonObject.getString("name"),
+                email = jsonObject.getString("email")
+            )
+        )
+    }
+    return users
+}
